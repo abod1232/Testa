5f15c131d58ea78e8261d80494161f6fc92099ca
abod1232 <abgost4@outlook.com>
2026-09-20 20:10:47 +0300

    Update delay duration in Shahid4u.kt
    Reduced delay from 30 seconds to 25 seconds.

--- Shahid4u/src/main/kotlin/com/shahid4u/Shahid4u.kt
+++ Shahid4u/src/main/kotlin/com/shahid4u/Shahid4u.kt
@@ -312,7 +312,7 @@ class Shahid4u : MainAPI() {
                     "تم تنفيذ 5 طلبات، انتظار 4 ثوانٍ قبل المتابعة..."
                 )
 
-                kotlinx.coroutines.delay(30000L)
+                kotlinx.coroutines.delay(25000L)
             }
 
             val seasonUrl = seasonElement.attr("href")
