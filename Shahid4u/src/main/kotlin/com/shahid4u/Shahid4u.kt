7ad11319678ced550f3c2bef6e0a48efc9b3ac0f
abod1232 <abgost4@outlook.com>
2026-09-20 20:31:12 +0300

    Reduce delay from 20 seconds to 15 seconds

--- Shahid4u/src/main/kotlin/com/shahid4u/Shahid4u.kt
+++ Shahid4u/src/main/kotlin/com/shahid4u/Shahid4u.kt
@@ -312,7 +312,7 @@ class Shahid4u : MainAPI() {
                     "تم تنفيذ 5 طلبات، انتظار 4 ثوانٍ قبل المتابعة..."
                 )
 
-                kotlinx.coroutines.delay(20000L)
+                kotlinx.coroutines.delay(15000L)
             }
 
             val seasonUrl = seasonElement.attr("href")
